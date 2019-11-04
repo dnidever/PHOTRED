f133197e86178bd916f37375802e7586f5959623
David Nidever <dnidever@noao.edu>
2019-11-03 23:12:17 -0700

    Adding remsatstars.py

---
+++ scripts/remsatstars.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+#
+# Script for removing saturated stars (with some saturated pixels) from a DAOPHOT list
+
+import os
+import sys
+import numpy as np
+from astropy.io import fits
+
+def read(filename):
+    with open(filename) as fp:
+        contents = fp.read()
+    return contents
+
+def write(filename,lines):
+    if os.path.exists(filename): os.remove(filename)
+    # Write the file
+    f = open(filename,'w')
+    f.writelines(lines)
+    f.close()
+
+# Main command-line program
+if __name__ == "__main__":
+    # List filename
+    filename = sys.argv[1]
+    print('Input list '+filename)
+    base,ext = os.path.splitext(os.path.basename(filename))
+    if len(sys.argv)>2:
+        outfile = sys.argv[2]
+    else:
+        outfile = filename
+    # Load the list file
+    lines = read(filename)
+    lines = lines.split('\n')
+    nlines = len(lines)
+    if nlines<4:
+        raise Exception('No stars in '+filename)
+    slines = lines[3:]
+    if slines[-1]=='': del slines[-1]   # remove blank last line
+    if type(slines) is list:
+        nstars = len(slines)
+    else:
+        nstars = 1
+    # Load the opt file
+    if os.path.exists(base+'.opt') is False:
+        raise Exception(base+'.opt NOT FOUND')
+    optlines = read(base+'.opt')
+    optlines = optlines.split('\n')
+    if optlines[-1]=='': del optlines[-1] # remove blank last line 
+    if optlines[-1]=='': del optlines[-1] # remove blank last line 
+    ps = 10
+    for o in optlines:
+        if o.find('PS')>-1: ps=np.float(o.split('=')[1])
+        if o.find('HI')>-1: sat=np.float(o.split('=')[1])
+    rad = np.ceil(ps)
+    # Load the fits file
+    if os.path.exists(base+'.fits') is False:
+        raise Exception(base+'.fits NOT FOUND')
+    im, head = fits.getdata(base+'.fits',header=True)
+    ny, nx = im.shape
+    # Loop over the stars
+    badind = []
+    soutlines = []
+    for i in range(nstars):
+        slines1 = slines[i]
+        arr = slines1.split()
+        id, x, y, mag = arr[0:4]
+        x = np.float(x)
+        y = np.float(y)
+        xlo = np.int(np.max([np.round(x)-rad,0]))
+        xhi = np.int(np.min([np.round(x)+rad,nx]))
+        ylo = np.int(np.max([np.round(y)-rad,0]))
+        yhi = np.int(np.min([np.round(y)+rad,ny]))
+        subim = im[ylo:yhi,xlo:xhi]
+        nbad = np.sum(subim>=sat)
+        # Some saturated pixels, remove
+        if nbad>0:
+            badind.append(i)
+            print('Star '+str(id)+'  '+str(nbad)+' saturated pixels')
+        # No saturated pixels, keep
+        else:
+            soutlines.append(slines1)
+    # Create output lines
+    outlines = lines[0:3].copy()
+    if len(soutlines)>0: outlines+=soutlines
+    outlines += ['']
+    outline = '\n'.join(outlines)
+    # Write to output file
+    if os.path.exists(outfile): os.remove(outfile)
+    print('Writing to output list '+outfile)
+    write(outfile,outline)
