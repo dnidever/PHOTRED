6a4551dbb021480dce59ae15232f269cb43de628
David Nidever <dnidever@lsst.org>
2022-04-12 16:49:24 -0600

    updates to allframe.py

--- python/allframe.py
+++ python/allframe.py
@@ -18,8 +18,6 @@ from dlnpyutils import utils as dln
 def loadsetup():
     pass
 
-def loadmch():
-    pass
 
 def loadmch(mchfile):
     """
@@ -44,8 +42,6 @@ def loadmch(mchfile):
     Translated to Python by D. Nidever,  April 2022
     """ 
      
-    count = 0 
-     
     # Test the file 
     if os.path.exists(mchfile)==False:
         raise ValueError(mchfile+' NOT FOUND')
@@ -55,7 +51,6 @@ def loadmch(mchfile):
 
     # Creating the trans array
     lines2 = [re.sub("'","",l) for l in lines]
-    #lines2 = repstr(lines,"'",'') 
     nlines = len(lines)
 
     arr = lines2[0].split()
@@ -75,6 +70,191 @@ def loadmch(mchfile):
         magoff[i,:] = arr[ntrans+1:]
  
     return files,trans,magoff
+
+def loadmakemag(filename):
+    """"
+    This loads the .makemag file output in ALLFRAME.PRO. 
+    This used to be handled by LOADRAW.PRO, but they diverged. 
+    This is basically the old version of loadraw.pro. 
+     
+    Parameters
+    ----------
+    filename   The name of the .makemag file 
+     
+    Returns
+    -------
+    phot       A structure with the makemag data 
+    head       A string array with the makemag header 
+     
+    Example
+    ------
+    
+    phot,head = loadmakemag('temp.makemag')
+     
+    By D. Nidever   Feb. 2008 (basically a copy of loadals.pro 
+    Translated to Python by D. Nidever,  April 2022
+    """
+     
+    if os.path.exists(filename)==False:
+        raise ValueError(filename+' DOES NOT EXIST')
+     
+    # Is this an ALS or PHOT file
+    with open(filename,'rb') as f:
+        line1 = f.readline()
+        line2 = f.readline()
+        line3 = f.readline()
+     
+    # This is an ALS file
+    arr1 = line1.split()
+    if arr1[0]=='NL' and line3.strip()=='': 
+        # Figure out the number of columns
+        f = open(filename,'rb')
+        line1 = f.readline()
+        line2 = f.readline()
+        line3 = f.readline()        
+         
+        # First line for the first star
+        line4 = f.readline()
+        instr = line4 
+         
+        # Check for continuation lines 
+        endflag = 0 
+        nstarline = 1 
+        continuation = 0 
+        while (endflag != 1) and ~eof(unit): 
+            line4 = f.readline()
+         
+            # If there are too many frames/columns then these 
+            # go on separate lines and lead with 27 blank spaces 
+         
+            # This is a continuation line 
+            if line4[0:16]=='':
+                trial = line4[33:34]
+                if trial == ' ': 
+                    nspaces = 24 
+                else: 
+                    nspaces = 25
+                instr1 = line4[nspaces:]
+                instr += instr1 
+                nstarline += 1 
+                continuation = 1 
+            else:
+                endflag = 1 
+        f.close()
+         
+        # Now parts the long line for a single star with a formatted read 
+        #fmt = '(I7,2A9,'+strtrim(2*nmag+2,2)+'F9.4)' 
+        nmag = (len(instr)-(7+2*9+2*9)) / 9 / 2 
+        ncol = nmag*2+5 
+         
+        # ID  X  Y  MAG1  ERR1  MAG2  ERR2 ...  CHI SHARP 
+        #nmag = (ncol-5)/2 
+         
+        # Stars in this file 
+        numstar = (FILE_LINES(filename)-3 )/nstarline 
+         
+        # LOADING THE DATA 
+        #------------------ 
+         
+        # nfiles < 12 
+        # Each star has data on 1 line 
+        #if (nmag lt 12) then begin 
+        if (continuation == 0): 
+             
+            fieldtypes = [3,lonarr(ncol-1)+4] 
+            fieldnames = ['ID','X','Y'] 
+            for i in np.arange(1,nmag+1): 
+                fieldnames = [fieldnames,'MAG'+str(i,2),'ERR'+str(i,2)] 
+            fieldnames = [fieldnames,'CHI','SHARP'] 
+
+            phot = Table.read(filename)
+            #phot = importascii(filename,fieldtype=fieldtypes,fieldnames=fieldnames,skip=3,/noprint) 
+            head = [line1,line2] 
+             
+            # nfiles >= 12 
+            # Each star has data on 2 lines 
+            # Most of this code was copied from photcalib.pro 
+        else: 
+             
+            # mastable is where everything is stored, id, x, y, unsolved magnitudes, chi, sharp
+            mastable = np.zeros((numstar,2*nmag+5),float)
+             
+            # Reading in the magnitude file
+            f = open(filename,'rb')
+            line1 = f.readline()
+            line2 = f.readline()
+            line3 = f.readline()
+            head = [line1,line2] 
+             
+            # Loop through the stars 
+            for j in np.arange(numstar): 
+                instr = '' 
+                instr1 = ' '
+                inline = np.zeros(2*nmag+5,float)
+                 
+                # Loop through the lines per star 
+                for k in np.arange(nstarline):
+                    instr1 = f.readline()
+                    if k > 0: 
+                        # There are leading spaces, 24 or 25 
+                        # Use the first character AFTER the first column to figure out 
+                        #   how many spaces we need to strip off 
+                        # The MAG/ERR columns are F9.4, so if there are 25 spaces then 
+                        #   the 34th character (index=33) should be the last digit of MAG1 (25+9=34) 
+                        #   and be a number.  If there are 24 leading space then the 
+                        #   34th character will right after MAG1 and be a space.
+                        trial = instr1[33:34]
+                        if trial == ' ': 
+                            nspaces = 24 
+                        else: 
+                            nspaces = 25 
+                        instr1 = instr1[nspaces:]
+                    #if k gt 0 then instr1=strmid(instr1,25) ; 2nd and later lines have 25 leading spaces 
+                    instr += instr1 
+                 
+                # WRITE (1,111) IDMAST(IMASTR), POSIT1, POSIT2, 
+                # .            ((DATUM(J,I), J=1,2), I=1,NFRM), SUMCHI, SUMSHP 
+                #        111       FORMAT (I7, 2A9, 12F9.4:/ (25X, 12F9.4)) 
+                 
+                # formatted read 
+                fmt = '(I7,2A9,'+str(2*nmag+2,2)+'F9.4)' 
+                id = 0
+                x=''
+                y=''
+                all=np.zeros(2*nmag+2,float) 
+                reads,instr,id,x,y,all,format=fmt 
+                inline[0] = id 
+                inline[1] = x 
+                inline[2] = y 
+                inline[3:2*nmag+5-1] = all 
+                 
+                # old method, causes problems when there are no spaces between values 
+                #reads,instr,inline 
+                 
+                mastable[j,0:2*nmag+5-1] = inline[0:2*nmag+5-1] 
+             
+            f.close()
+             
+            # Now transfer to structure 
+            fieldtypes = [3,lonarr(nmag-1)+4] 
+            fieldnames = ['ID','X','Y'] 
+            for i in np.arange(1,nmag+1): 
+                fieldnames = [fieldnames,'MAG'+str(i,2),'ERR'+str(i,2)] 
+            fieldnames = [fieldnames,'CHI','SHARP'] 
+             
+            mastable2 = transpose(mastable) 
+            if numstar > 1: 
+                phot = ARR2STR(mastable2,fieldnames=fieldnames,fieldtypes=fieldtypes)
+            else: 
+                # arr2str needs a 2D array, make it [Ncol, 1] 
+                phot = ARR2STR(reform(mastable2,len(mastable2),1),fieldnames=fieldnames,fieldtypes=fieldtypes)
+                
+    # Not a raw/makemag file 
+    else: 
+        print('This is NOT a MAKEMAG file')
+        return None,None
+
+    return phot,head,mastable
 
 
 def readpar(array,keyword):
