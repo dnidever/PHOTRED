98b974c979afba74f75d28db5ed31b7839f12c27
David Nidever <dnidever@lsst.org>
2022-04-12 19:13:45 -0600

    adding setup.py and utils.py

---
+++ python/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ["utils","allframe"]
+__version__ = '1.0.0'
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name='PHOTRED',
+      version='1.0.0',
+      description='Generic PSF Photometry Pipeline',
+      author='David Nidever',
+      author_email='dnidever@montana.edu',
+      url='https://github.com/dnidever/PHOTRED',
+      packages=find_packages(exclude=["tests"]),
+      #scripts=['bin/photred'],
+      install_requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils(>=1.0.3)']
+      #include_package_data=True,
+)
