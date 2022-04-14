7632feff0efc2456489164d6e63e23bfa124a345
David Nidever <dnidever@lsst.org>
2022-04-14 13:52:56 -0600

    update to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='photred',
       author='David Nidever',
       author_email='dnidever@montana.edu',
       url='https://github.com/dnidever/PHOTRED',
-      packages=find_packages(exclude=["tests"]),
+      packages=['photred'],
       #scripts=['bin/photred'],
       install_requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils(>=1.0.3)']
       #include_package_data=True,
